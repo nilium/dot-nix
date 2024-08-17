2de857161b777559e05ccf3c35bc532e52beb970
Noel <ncower@nil.dev>
2024-08-17 14:31:07 -0700

    Switch Helix flake back to main repo
    Took me a bit of reading the flake to figure out there's a way to
    regenerate the wrapper from a patched build, via the `wrapper`
    passthru, so using that now and adding my patch and the one I
    cherry-picked to fix pipes. This should hopefully make it unnecessary
    to continue maintaining patched branches of Helix itself.

--- home/editors/helix/default.nix
+++ home/editors/helix/default.nix
@@ -8,7 +8,18 @@
     programs.helix = {
       enable = true;
       defaultEditor = true;
-      package = helix.packages.${pkgs.system}.helix;
+      package = let
+        helix-pkgs = helix.packages.${pkgs.system};
+        helix-patched = helix-pkgs.helix-unwrapped.overrideAttrs (prev: {
+          patches =
+            (prev.patches or [])
+            ++ [
+              ./ruler-order.patch #
+              ./pipe-trim.patch # Remove after 24.07 (https://github.com/helix-editor/helix/commit/535351067c2ac018ee2fef6cc685f49065617bd1)
+            ];
+        });
+      in
+        helix-pkgs.helix.passthru.wrapper helix-patched;
 
       themes = {
         big-duo = import ./big-duo.nix;
